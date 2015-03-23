0346a0463397b09b1802a445613ecaa265562d99
galassoa <alessandro.galasso@gmail.com>
2015-03-23 22:38:16 +0100

    First commit (version 2)

---
+++ 2.0/localfilesystem-sequences-storage/src/com/galassiasoft/uhopper/LocalFilesystemSequencesStorage.java
@@ -0,0 +1,29 @@
+package com.galassiasoft.uhopper;
+
+import java.util.ArrayList;
+
+public class LocalFilesystemSequencesStorage implements SequencesStorage {
+
+	@Override
+	public void saveSequence(Sequence sequence) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayList<Sequence> searchSequence(Long from, Long to, Integer sensor_id) {
+		//Not implemented
+		return null;
+	}
+
+	@Override
+	public String getInfo() {
+		return "Local filesystem";
+	}
+
+	@Override
+	public void dispose() {
+		// Disposing objects
+	}
+
+}
