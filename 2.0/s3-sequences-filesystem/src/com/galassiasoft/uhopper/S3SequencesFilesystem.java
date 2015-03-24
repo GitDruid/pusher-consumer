2ee9ad2d56462c0948c0d3be2484044362b7ab1d
galassoa <alessandro.galasso@gmail.com>
2015-03-24 11:27:40 +0100

    First implementation

--- 2.0/s3-sequences-filesystem/src/com/galassiasoft/uhopper/S3SequencesFilesystem.java
+++ 2.0/s3-sequences-filesystem/src/com/galassiasoft/uhopper/S3SequencesFilesystem.java
@@ -1,23 +1,59 @@
 package com.galassiasoft.uhopper;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.owlike.genson.Genson;
+
 public class S3SequencesFilesystem implements SequencesFilesystem {
 
+	private AmazonS3Client s3Client;
+	
+	public S3SequencesFilesystem() {
+		this("localhost", 4569, "foo", "bar");
+	}
+	
+	public S3SequencesFilesystem(String host, int port, String accessKey, String secretKey) {
+		BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+		AmazonS3Client s3Client = new AmazonS3Client(credentials);
+		s3Client.setEndpoint("http://" + host + ":" + port);
+		s3Client.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
+		try {
+			s3Client.createBucket("com.galassiasoft.bucket");
+		} catch (AmazonServiceException e) {
+			//already created
+		}
+	}
+	
 	@Override
-	public void saveFile() {
-		// TODO Auto-generated method stub
+	public void saveToFile(Sequence sequence, String filename) {
+		String jsonSequence = new Genson().serialize(sequence);
 		
+		ObjectMetadata om = new ObjectMetadata();
+		om.setContentType("application/json");
+		om.setContentLength(jsonSequence.length());
+		
+		s3Client.putObject("com.galassiasoft.bucket", "longest-sequence", getStream(jsonSequence), om);
 	}
 
 	@Override
 	public String getInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		return "AWS S3 (" + s3Client.getServiceName() + ")";
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
 	}
 
+	private static InputStream getStream(String s) {
+		return new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8));
+	}
+	
 }
